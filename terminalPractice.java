e714a7687a950a0b26890bc06f20dfce6d754068
vsh <vish.sriramoju@gmail.com>
2021-12-01 22:37:51 +0530

    *****

--- terminalPractice.java
+++ terminalPractice.java
@@ -3,6 +3,8 @@ public class terminalPractice {
         System.out.println("print on terminal- hello world");
         System.out.println("added new line update to GitHub");
         System.out.println("polling for Jenkins");
+        System.out.println("continuous polling for Jenkins");
+
 
 
     }
