5aa14456c00ea6681befb565dba1c6c293b6861a
vsh <vish.sriramoju@gmail.com>
2021-12-01 22:33:45 +0530

    this is commit message

--- terminalPractice.java
+++ terminalPractice.java
@@ -2,6 +2,8 @@ public class terminalPractice {
     public static void main(String[] args) {
         System.out.println("print on terminal- hello world");
         System.out.println("added new line update to GitHub");
+        System.out.println("polling for Jenkins");
+
 
     }
 }
