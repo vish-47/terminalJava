daff21800b78dffe1a2f2170485afd22f5516de1
vsh <vish.sriramoju@gmail.com>
2021-12-01 22:18:07 +0530

    added new file

--- terminalPractice.java
+++ terminalPractice.java
@@ -1,5 +1,7 @@
 public class terminalPractice {
     public static void main(String[] args) {
         System.out.println("print on terminal- hello world");
+        System.out.println("added new line update to GitHub");
+
     }
 }
