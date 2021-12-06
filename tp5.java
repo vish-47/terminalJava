b99846223fa2265764923b8aafc91e4740841658
vsh <vish.sriramoju@gmail.com>
2021-12-06 21:14:30 +0530

    push to diff rem branch

---
+++ tp5.java
@@ -0,0 +1,13 @@
+public class terminalPractice {
+    public static void main(String[] args) {
+        System.out.println("print on terminal- hello world");
+	System.out.println("changed, deleted one file again line to create conflict during merging");
+        System.out.println("polling for Jenkins");
+        System.out.println("continuous polling for Jenkins");
+        System.out.println("pushing to remote branch");
+
+
+
+
+    }
+}
