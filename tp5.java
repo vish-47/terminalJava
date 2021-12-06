a14f6fb92772f9e3bfd06adbad58b0a57bf9dfdb
vish-47 <78438560+vish-47@users.noreply.github.com>
2021-12-06 21:34:36 +0530

    Update tp5.java

--- tp5.java
+++ tp5.java
@@ -2,10 +2,9 @@ public class terminalPractice {
     public static void main(String[] args) {
         System.out.println("print on terminal- hello world");
 	System.out.println("changed, deleted one file again line to create conflict during merging");
-        System.out.println("polling for Jenkins");
         System.out.println("continuous polling for Jenkins");
         System.out.println("pushing to remote branch");
-	            System.out.println("changed on remote branch2");
+	            System.out.println("changed on sameline on remote branch2");
 
 
 
